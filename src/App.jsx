9b79d953d4057960df992b16fd210949c2546f0a
MaiEbrahim-S <maiebrahima616@gmail.com>
2026-08-07 16:41:39 +0300

    fix page

--- src/App.jsx
+++ src/App.jsx
@@ -39,7 +39,7 @@ function AppRoutes() {
 function App() {
   return (
     <AuthProvider>
-      <BrowserRouter>
+      <BrowserRouter basename="/Shopping-E-commerce/">
         <AppRoutes />
       </BrowserRouter>
     </AuthProvider>
